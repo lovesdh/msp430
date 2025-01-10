c98d8a33d81b9dacee11a6a4ad17ed65b479156b
MISAKA <145181699+lovesdh@users.noreply.github.com>
2025-01-10 15:48:06 +0800

    Rename ADC_Volt(单次触发）.c to ADC_Volt_single.c

--- ADC温度电压采集/ADC_Volt_single.c
+++ ADC温度电压采集/ADC_Volt_single.c
@@ -2,56 +2,56 @@
 #include "stdint.h"
 /*
  * @fn:		void InitSystemClock(void)
- * @brief:	��ʼ��ϵͳʱ��
+ * @brief:	初始化系统时钟
  * @para:	none
  * @return:	none
- * @comment:��ʼ��ϵͳʱ��
+ * @comment:初始化系统时钟
  */
 void InitSystemClock(void)
 {
-	/*����DCOΪ1MHz*/
+	/*配置DCO为1MHz*/
     DCOCTL = CALDCO_1MHZ;
     BCSCTL1 = CALBC1_1MHZ;
-    /*����SMCLK��ʱ��ԴΪDCO*/
+    /*配置SMCLK的时钟源为DCO*/
     BCSCTL2 &= ~SELS;
-    /*SMCLK�ķ�Ƶϵ����Ϊ1*/
+    /*SMCLK的分频系数置为1*/
     BCSCTL2 &= ~(DIVS0 | DIVS1);
 }
 /*
  * @fn:		void InitUART(void)
- * @brief:	��ʼ�����ڣ��������ò����ʣ�����λ��У��λ��
+ * @brief:	初始化串口，包括设置波特率，数据位，校验位等
  * @para:	none
  * @return:	none
- * @comment:��ʼ������
+ * @comment:初始化串口
  */
 void InitUART(void)
 {
-    /*��λUSCI_Ax*/
+    /*复位USCI_Ax*/
     UCA0CTL1 |= UCSWRST;
 
-    /*ѡ��USCI_AxΪUARTģʽ*/
+    /*选择USCI_Ax为UART模式*/
     UCA0CTL0 &= ~UCSYNC;
 
-    /*����UARTʱ��ԴΪSMCLK*/
+    /*配置UART时钟源为SMCLK*/
     UCA0CTL1 |= UCSSEL1;
 
-    /*���ò�����Ϊ9600@1MHz*/
+    /*配置波特率为9600@1MHz*/
     UCA0BR0 = 0x68;
     UCA0BR1 = 0x00;
     UCA0MCTL = 1 << 1;
-    /*ʹ�ܶ˿ڸ���*/
+    /*使能端口复用*/
     P1SEL |= BIT1 + BIT2;
     P1SEL2 |= BIT1 + BIT2;
-    /*�����λλ��ʹ��UART*/
+    /*清除复位位，使能UART*/
     UCA0CTL1 &= ~UCSWRST;
 }
 /*
  * @fn:		void UARTSendString(uint8_t *pbuff,uint8_t num)
- * @brief:	ͨ�����ڷ����ַ���
- * @para:	pbuff:ָ��Ҫ�����ַ�����ָ��
- * 			num:Ҫ���͵��ַ�����
+ * @brief:	通过串口发送字符串
+ * @para:	pbuff:指向要发送字符串的指针
+ * 			num:要发送的字符个数
  * @return:	none
- * @comment:ͨ�����ڷ����ַ���
+ * @comment:通过串口发送字符串
  */
 void UARTSendString(uint8_t *pbuff,uint8_t num)
 {
@@ -64,10 +64,10 @@ void UARTSendString(uint8_t *pbuff,uint8_t num)
 }
 /*
  * @fn:		void PrintNumber(uint16_t num)
- * @brief:	ͨ�����ڷ�������
- * @para:	num:����
+ * @brief:	通过串口发送数字
+ * @para:	num:变量
  * @return:	none
- * @comment:ͨ�����ڷ�������
+ * @comment:通过串口发送数字
  */
 void PrintNumber(uint16_t num)
 {
@@ -82,10 +82,10 @@ void PrintNumber(uint16_t num)
 }
 /*
  * @fn:		void PrintFloat(float num)
- * @brief:	ͨ�����ڷ��͸�����
- * @para:	num:����
+ * @brief:	通过串口发送浮点数
+ * @para:	num:变量
  * @return:	none
- * @comment:ͨ�����ڷ��͸��������ɷ���1λ����λ+3λС��λ
+ * @comment:通过串口发送浮点数，可发送1位整数位+3位小数位
  */
 void PrintFloat(float num)
 {
@@ -99,48 +99,48 @@ void PrintFloat(float num)
 }
 /*
  * @fn:		void InitADC(void)
- * @brief:	��ʼ��ADC
+ * @brief:	初始化ADC
  * @para:	none
  * @return:	none
- * @comment:��ʼ��ADC
+ * @comment:初始化ADC
  */
 void InitADC(void)
 {
-	  /*����ADCʱ��MCLK*/
+	  /*设置ADC时钟MCLK*/
 	  ADC10CTL1 |= ADC10SSEL_2;
-	  /*ADC 2��Ƶ*/
+	  /*ADC 2分频*/
 	  ADC10CTL1 |= ADC10DIV_0;
-	  /*����ADC��׼Դ*/
+	  /*设置ADC基准源*/
 	  ADC10CTL0 |= SREF_1;
-	  /*����ADC��������ʱ��64CLK*/
+	  /*设置ADC采样保持时间64CLK*/
 	  ADC10CTL0 |= ADC10SHT_3;
-	  /*����ADC������200k*/
+	  /*设置ADC采样率200k*/
 	  ADC10CTL0 &= ~ADC10SR;
-	  /*ADC��׼ѡ��2.5V*/
+	  /*ADC基准选择2.5V*/
 	  ADC10CTL0 |= REF2_5V;
-	  /*������׼*/
+	  /*开启基准*/
 	  ADC10CTL0 |= REFON;
-	  /*ѡ��ADC����ͨ��A0*/
+	  /*选择ADC输入通道A0*/
 	  ADC10CTL1 |= INCH_0;
-	  /*����A0ģ������*/
+	  /*允许A0模拟输入*/
 	  ADC10AE0 |= 0x0001;
-	  /*����ADC*/
+	  /*开启ADC*/
 	  ADC10CTL0 |= ADC10ON;
 }
 /*
  * @fn:		uint16_t GetADCValue(void)
- * @brief:	����һ��ADCת��������ADCת�����
+ * @brief:	进行一次ADC转换并返回ADC转换结果
  * @para:	none
- * @return:	ADCת�����
- * @comment:ADCת�����Ϊ10bit����uint16_t���ͷ��أ���10λΪ��Ч����
+ * @return:	ADC转换结果
+ * @comment:ADC转换结果为10bit，以uint16_t类型返回，低10位为有效数据
  */
 uint16_t GetADCValue(void)
 {
-	  /*��ʼת��*/
+	  /*开始转换*/
 	  ADC10CTL0 |= ADC10SC|ENC;
-	  /*�ȴ�ת�����*/
+	  /*等待转换完成*/
 	  while(ADC10CTL1&ADC10BUSY);
-	  /*���ؽ��*/
+	  /*返回结果*/
 	  return ADC10MEM;
 }
 /*
@@ -159,9 +159,9 @@ int main(void)
     	adcvalue = GetADCValue();
     	voltage = adcvalue * 2.5 / 1023;
 		if(voltage>1.0){
-			UARTSendString("ADC10ת�����Ϊ��",17);
+			UARTSendString("ADC10转换结果为：",17);
 			PrintNumber(adcvalue);
-			UARTSendString("��Ӧ��ѹֵΪ��",14);
+			UARTSendString("相应电压值为：",14);
 			PrintFloat(voltage);
 			__delay_cycles(300000);
 		}
